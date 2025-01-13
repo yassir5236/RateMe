1b3f875a1aefb43219eff96f26be1fa6e13a3e78
yassir5236 <yassiraitlahmadi@gmail.com>
2025-01-13 09:19:03 +0100

    first commit

---
+++ RateMe/src/main/java/com/yassir/RateMe/RateMeApplication.java
@@ -0,0 +1,15 @@
+package com.yassir.RateMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMeApplication.class, args);
+		System.out.println("hello rate Me");
+	}
+
+
+}
---
+++ RateMe/src/test/java/com/yassir/RateMe/RateMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassir.RateMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
